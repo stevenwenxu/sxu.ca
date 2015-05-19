20b77d3d95f541fbcfedee0aa78e66ffba5c6fab
Steven-xu94 <steven.xu13@gmail.com>
2015-05-19 07:49:54 -0700

    Adjusting ports

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ var app     = express();
 app.use(express.static(__dirname));
 app.use('/scripts', express.static(__dirname + '/node_modules'));
 
+app.set('port', (process.env.PORT || 5000));
 app.get('/', function(req, res) {
    res.sendFile('index.html');
 });
 
 app.get('/blog', function(req, res) {
    res.send('Migrating...');
+});
+
+app.listen(app.get('port'), function() {
+   console.log('Listening on ', app.get('port'));
 });
 
