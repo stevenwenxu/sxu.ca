acb6a941e46cb12b8b64dc381ce061765c910c41
Steven-xu94 <steven.xu13@gmail.com>
2015-06-05 22:23:59 -0700

    trying to fix blog

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use('/scripts', express.static(__dirname + '/node_modules'));
 app.use('/styles', express.static(__dirname + '/styles'));
 app.use('/pictures', express.static(__dirname + '/pictures'));
 app.use('/js', express.static(__dirname + '/js'));
-app.use('/blog', express.static(__dirname + '/../blog/_site'));
+app.use('/blog', express.static(__dirname + '/blog/_site'));
 app.use('/files', express.static(__dirname + '/files'))
 
 app.set('port', (process.env.PORT || 5000));
@@ -16,7 +16,7 @@ app.get('/', function(req, res) {
 });
 
 app.get('/blog', function(req, res) {
-   res.sendFile('index.html', { root: 'blog/_site' });
+   res.sendFile('index.html', { root: '../blog/_site' });
 });
 
 app.listen(app.get('port'), function() {
