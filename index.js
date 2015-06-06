492bda3d81d220a607c136ddd2b24fca0113ccc5
Steven-xu94 <steven.xu13@gmail.com>
2015-06-05 21:16:21 -0700

    Everything is good on Heroku now\!

--- index.js
+++ index.js
@@ -3,16 +3,20 @@ var app     = express();
 
 var http = require('http');
 
-app.use(express.static(__dirname));
 app.use('/scripts', express.static(__dirname + '/node_modules'));
+app.use('/styles', express.static(__dirname + '/styles'));
+app.use('/pictures', express.static(__dirname + '/pictures'));
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/blog', express.static(__dirname + '/blog/_site'));
+app.use('/files', express.static(__dirname + '/files'))
 
 app.set('port', (process.env.PORT || 5000));
 app.get('/', function(req, res) {
-   res.sendFile('index.html');
+   res.sendFile('index.html', { root: __dirname });
 });
 
 app.get('/blog', function(req, res) {
-   res.send('Migrating...');
+   res.sendFile('index.html', { root: __dirname + '/blog/_site' });
 });
 
 app.listen(app.get('port'), function() {
